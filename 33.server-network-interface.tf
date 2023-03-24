5485906a6e84f947b3d2e37ad2c56c951fc4f1ad
Gyoung Min <73932913+kosaf1996@users.noreply.github.com>
2023-03-24 19:09:46 +0900

    Update 33.server-network-interface.tf

--- 33.server-network-interface.tf
+++ 33.server-network-interface.tf
@@ -1,7 +1,7 @@
-resource "ncloud_network_interface" "nic" {
-    for_each = var.Network-Interface
-        name                  = each.value["name"]
-        subnet_no             = ncloud_subnet.subnet[each.value["subnet"]].id
-        access_control_groups = [ncloud_access_control_group.acg[each.value["acg"]].id]
-        #private_ip            = each.value["private_ip"]
-}
+# resource "ncloud_network_interface" "nic" {
+#     for_each = var.Network-Interface
+#         name                  = each.value["name"]
+#         subnet_no             = ncloud_subnet.subnet[each.value["subnet"]].id
+#         access_control_groups = [ncloud_access_control_group.acg[each.value["acg"]].id]
+#         #private_ip            = each.value["private_ip"]
+# }

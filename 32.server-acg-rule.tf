650a2f064c246433d5cc551e77d5744bfac00449
Gyoung Min <73932913+kosaf1996@users.noreply.github.com>
2023-03-24 19:09:16 +0900

    Update 32.server-acg-rule.tf

--- 32.server-acg-rule.tf
+++ 32.server-acg-rule.tf
@@ -85,52 +85,52 @@ resource "ncloud_access_control_group_rule" "cidr-db" {
 # ##         ACG           ##
 # ############################
 
-#Public Subnet  
-resource "ncloud_access_control_group_rule" "acg-pub" {
-    access_control_group_no = ncloud_access_control_group.acg["vpc1-acg-pub"].id    
+# #Public Subnet  
+# resource "ncloud_access_control_group_rule" "acg-pub" {
+#     access_control_group_no = ncloud_access_control_group.acg["vpc1-acg-pub"].id    
 
-    dynamic "inbound" {
-        for_each = local.vpc1-pub-subnet-inbound-acg
-            content {
-                protocol    = inbound.value[0]
-                source_access_control_group_no = ncloud_access_control_group.acg[inbound.value["1"]].id 
-                port_range  = inbound.value[2]
-                description = inbound.value[3]
-            }
-     }
+#     dynamic "inbound" {
+#         for_each = local.vpc1-pub-subnet-inbound-acg
+#             content {
+#                 protocol    = inbound.value[0]
+#                 source_access_control_group_no = ncloud_access_control_group.acg[inbound.value["1"]].id 
+#                 port_range  = inbound.value[2]
+#                 description = inbound.value[3]
+#             }
+#      }
 
-    dynamic "outbound" {
-        for_each = local.vpc1-pub-subnet-outbound-acg 
-            content {
-                protocol    = outbound.value[0]
-                source_access_control_group_no = ncloud_access_control_group.acg[outbound.value["1"]].id 
-                port_range  = outbound.value[2]
-                description = outbound.value[3]
-            }
-     }
-}
+#     dynamic "outbound" {
+#         for_each = local.vpc1-pub-subnet-outbound-acg 
+#             content {
+#                 protocol    = outbound.value[0]
+#                 source_access_control_group_no = ncloud_access_control_group.acg[outbound.value["1"]].id 
+#                 port_range  = outbound.value[2]
+#                 description = outbound.value[3]
+#             }
+#      }
+# }
 
-#Private Subnet  
-resource "ncloud_access_control_group_rule" "acg-pri" {
-    access_control_group_no = ncloud_access_control_group.acg["vpc1-acg-pri"].id    
+# #Private Subnet  
+# resource "ncloud_access_control_group_rule" "acg-pri" {
+#     access_control_group_no = ncloud_access_control_group.acg["vpc1-acg-pri"].id    
 
-    dynamic "inbound" {
-        for_each = local.vpc1-pri-subnet-inbound-acg
-            content {
-                protocol    = inbound.value[0]
-                source_access_control_group_no = ncloud_access_control_group.acg[inbound.value["1"]].id 
-                port_range  = inbound.value[2]
-                description = inbound.value[3]
-            }
-     }
+#     dynamic "inbound" {
+#         for_each = local.vpc1-pri-subnet-inbound-acg
+#             content {
+#                 protocol    = inbound.value[0]
+#                 source_access_control_group_no = ncloud_access_control_group.acg[inbound.value["1"]].id 
+#                 port_range  = inbound.value[2]
+#                 description = inbound.value[3]
+#             }
+#      }
 
-    dynamic "outbound" {
-        for_each = local.vpc1-pri-subnet-outbound-acg 
-            content {
-                protocol    = outbound.value[0]
-                source_access_control_group_no = ncloud_access_control_group.acg[outbound.value["1"]].id 
-                port_range  = outbound.value[2]
-                description = outbound.value[3]
-            }
-     }
-}
+#     dynamic "outbound" {
+#         for_each = local.vpc1-pri-subnet-outbound-acg 
+#             content {
+#                 protocol    = outbound.value[0]
+#                 source_access_control_group_no = ncloud_access_control_group.acg[outbound.value["1"]].id 
+#                 port_range  = outbound.value[2]
+#                 description = outbound.value[3]
+#             }
+#      }
+# }
